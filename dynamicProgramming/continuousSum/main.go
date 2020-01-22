a63dcd1244bd5052671731ac3d3b9f96fcbfbbf4
dongun kim <>
2020-01-22 17:26:37 +0900

    - Add 연속합 알고리즘 (dynamic programming) - 맞음.

--- dynamicProgramming/continuousSum/main.go
+++ dynamicProgramming/continuousSum/main.go
@@ -8,8 +8,6 @@ import (
 	"strings"
 )
 
-var continuousInt []int
-
 func main() {
 	var t int
 
@@ -20,11 +18,10 @@ func main() {
 		panic("out of range")
 	}
 
-	continuousInt = make([]int, t)
+	continuousInt := make([]int, t)
 
 	reader := bufio.NewReader(os.Stdin)
 	str, _ := reader.ReadString('\n')
-
 	for index, v := range strings.Fields(str) {
 		if index >= len(continuousInt) {
 			break
@@ -32,5 +29,35 @@ func main() {
 		continuousInt[index], _ = strconv.Atoi(v)
 	}
 
-	fmt.Println(continuousInt)
+	sum := calSum(continuousInt)
+	fmt.Println(findMax(sum))
+}
+
+// calSum - values 연속된 합을 구해서 저장한 array 반환
+func calSum(values []int) (sum []int) {
+	c := -1001
+	sum = make([]int, 0)
+	for _, value := range values {
+		if c < 0 {
+			c = value
+		} else {
+			c += value
+			if c < 0 { // 합이 0보다 작을 경우 0으로 리셋
+				c = 0
+			}
+		}
+		sum = append(sum, c)
+	}
+	return
+}
+
+// findMax - array 값중 가장 큰 값을 반환
+func findMax(sum []int) (max int) {
+	max = -1001
+	for _, value := range sum {
+		if max < value {
+			max = value
+		}
+	}
+	return
 }
